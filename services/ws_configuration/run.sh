2ac317c15afd54192565d74e10774844f2d1897a
simone36050 <simone36050@gmail.com>
2018-07-03 18:37:47 +0200

    Add license to files header

--- services/ws_configuration/run.sh
+++ services/ws_configuration/run.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
 
+# This file is part of Flower.
+#
+# Copyright ©2018 Nicolò Mazzucato
+# Copyright ©2018 Antonio Groza
+# Copyright ©2018 Brunello Simone
+# Copyright ©2018 Alessio Marotta
+# DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+# 
+# Flower is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# Flower is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Flower.  If not, see <https://www.gnu.org/licenses/>.
+
 vm_host="10.10.3.1"
 
 echo "setup server $vm_host"
