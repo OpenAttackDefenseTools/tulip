24917a9a91fa41f2cfe646345149c623da259c75
Rick de Jager <rickdejager99@gmail.com>
2022-09-02 23:08:45 +0200

    Add tag correlation + zoom

--- frontend/src/components/Corrie.tsx
+++ frontend/src/components/Corrie.tsx
@@ -19,20 +19,22 @@ import { lastRefreshAtom } from "./Header";
 import ReactApexChart from "react-apexcharts";
 import { ApexOptions } from "apexcharts";
 
-import ReactDOMServer from "react-dom/server";
-
-import { FlowListEntry } from "./FlowList";
-
-import classes from "./FlowList.module.css";
-import classNames from "classnames";
+interface GraphProps {
+    flowList: Flow[];
+    mode: string;
+    searchParams: URLSearchParams;
+    setSearchParams: (a: URLSearchParams) => void;
+    onClickNavigate: (a: string) => void;
+}
 
 export const Corrie = () => {
-    let [searchParams, setSearchParams] = useSearchParams();
     let params = useParams();
 
     const { services, api, getFlows } = useTulip();
 
     const [flowList, setFlowList] = useState<Flow[]>([]);
+
+    const [searchParams, setSearchParams] = useSearchParams();
 
     const service_name = searchParams.get(SERVICE_FILTER_KEY) ?? "";
     const service = services.find((s) => s.name == service_name);
@@ -49,10 +51,6 @@ export const Corrie = () => {
     const [loading, setLoading] = useState(false);
 
     const [lastRefresh, setLastRefresh] = useAtom(lastRefreshAtom);
-
-    const navigate = useNavigate();
-    const onClickNavicate = useCallback((loc: string) => navigate(loc, { replace: true }), [navigate]);
-
 
     useEffect(() => {
         const fetchData = async () => {
@@ -89,6 +87,64 @@ export const Corrie = () => {
     ]);
 
     const mode = searchParams.get("correlation") ?? "time";
+    const setCorrelationMode = (mode: string) => {
+        searchParams.set(CORRELATION_MODE_KEY, mode);
+        setSearchParams(searchParams);
+    }
+
+    const inactiveButtonClass = "bg-blue-100 text-gray-800 rounded-md px-2 py-1";
+    const activeButtonClass = `${inactiveButtonClass} ring-2 ring-gray-500`;
+
+    const navigate = useNavigate();
+    const onClickNavigate = useCallback((loc: string) => navigate(loc, { replace: true }), [navigate]);
+
+    const graphProps: GraphProps = {
+        flowList: flowList,
+        mode: mode,
+        searchParams: searchParams,
+        setSearchParams: setSearchParams,
+        onClickNavigate: onClickNavigate,
+    }
+
+    return (
+        <div>
+            <div className="flex flex-col h-full">
+                <div className="text-sm bg-white border-b-gray-300 border-b shadow-md flex flex-col">
+                    <div className="p-2 flex space-x-2" style={{ height: 50 }}>
+                        <a className="text-center px-2 py-2">Correlation mode: </a>
+                        <button className={mode == "time" ? activeButtonClass : inactiveButtonClass}
+                                onClick={() => setCorrelationMode("time")}>
+                            time
+                        </button>
+                        <button className={mode == "packets" ? activeButtonClass : inactiveButtonClass}
+                                onClick={() => setCorrelationMode("packets")}>
+                            packets
+                        </button>
+                        <button className={mode == "tags" ? activeButtonClass : inactiveButtonClass}
+                                onClick={() => setCorrelationMode("tags")}>
+                            tags
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <div>
+            </div>
+            <div>
+                {(mode == "packets" || mode == "time") && TimePacketGraph(graphProps)}
+                {(mode == "tags") && TagGraph(graphProps)}
+            </div>
+        </div>
+    );
+}
+
+
+
+function TimePacketGraph(graphProps: GraphProps) {
+    const flowList = graphProps.flowList;
+    const mode = graphProps.mode;
+    const searchParams = graphProps.searchParams;
+    const setSearchParams = graphProps.setSearchParams;
+    const onClickNavigate = graphProps.onClickNavigate;
 
     const series: ApexAxisChartSeries = [{
         name: 'Flows',
@@ -130,70 +186,103 @@ export const Corrie = () => {
                     // Retrieve flowList from chart's labels. This is hacky, refer to FIXME above.
                     const flowIdList = config.w.config.labels;
                     const flow = flowIdList[config.dataPointIndex];
-                    onClickNavicate(`/flow/${flow}?${searchParams}`);
+                    onClickNavigate(`/flow/${flow}?${searchParams}`);
+                },
+                beforeZoom: function(chartContext, { xaxis }) {
+                    const start = Math.floor(xaxis.min);
+                    const end = Math.ceil(xaxis.max);
+                    searchParams.set(START_FILTER_KEY, start.toString())
+                    searchParams.set(END_FILTER_KEY, end.toString())
+                    setSearchParams(searchParams);
                 }
             }
         },
-        /*
-        tooltip: {
-            followCursor: true,
-            // TODO; these types are hacky
-            custom: function ({ dataPointIndex, w}: {dataPointIndex: number, w: any}) {
-                // Display corresponding flow like in the sidebar
-                const flowList = w.config.labels;
-                const flow = flowList[dataPointIndex];
-                const element = (
-                    <div className={classNames({
-                        [classes.list_container]: true,
-                    })}>
-                        <FlowListEntry
-                            key={flow._id.$oid}
-                            flow={flow}
-                            isActive={flow._id.$oid === params.id}
-                            onHeartClick={() => { }}
-                        />
-                    </div>
-                );
-                return ReactDOMServer.renderToString(element);;
-            },
-        }
-        */
     };
 
-    const setCorrelationMode = (mode: string) => {
-        searchParams.set(CORRELATION_MODE_KEY, mode);
-        setSearchParams(searchParams);
+    return (
+        <ReactApexChart
+            options={options}
+            series={series}
+            type="scatter"
+        />
+    )
+}
+
+
+function TagGraph(graphProps: GraphProps) {
+    const flowList = graphProps.flowList;
+    const mode = graphProps.mode;
+    const searchParams = graphProps.searchParams;
+    const setSearchParams = graphProps.setSearchParams;
+
+    function chunkData(flowList: Flow[]) {
+        let ret: any = [];
+        let ts = 0;
+        let acc = 0;
+        const window_size = 30000;
+        flowList.forEach((flow) => {
+            if (ts == 0) {ts = flow.time}
+
+            if (ts - flow.time> window_size) {
+                ret.push({"x": ts, "y": acc})
+                ts = 0;
+                acc = 0;
+            } else {
+                if (flow.tags.includes("flag-out")) {
+                    acc++;
+                }
+            }
+        });
+        
+        return ret;
     }
 
-    const inactiveButtonClass = "bg-blue-100 text-gray-800 rounded-md px-2 py-1";
-    const activeButtonClass = `${inactiveButtonClass} ring-2 ring-gray-500`;
+    const series_out: ApexAxisChartSeries = [{
+        name: 'Flag-out',
+        data:  chunkData(flowList)
+    }];
+
+    const options: ApexOptions = {
+        dataLabels: {
+            enabled: false
+        },
+        grid: {
+            xaxis: {
+                lines: {
+                    show: true
+                }
+            },
+            yaxis: {
+                lines: {
+                    show: true
+                }
+            },
+        },
+        xaxis: {
+            type: 'datetime', // FIXME: Timezone is not displayed correctly
+        },
+        labels: flowList.map((flow) => { return flow._id.$oid; }),
+        chart: {
+            animations: {
+                enabled: false
+            },
+            events: {
+                  beforeZoom: function(chartContext, { xaxis }) {
+                    const start = Math.floor(xaxis.min);
+                    const end = Math.ceil(xaxis.max);
+                    searchParams.set(START_FILTER_KEY, start.toString())
+                    searchParams.set(END_FILTER_KEY, end.toString())
+                    setSearchParams(searchParams);
+                }
+            }
+        },
+    };
 
     return (
-        <div>
-            <div className="flex flex-col h-full">
-                <div className="text-sm bg-white border-b-gray-300 border-b shadow-md flex flex-col">
-                    <div className="p-2 flex space-x-2" style={{ height: 50 }}>
-                        <a className="text-center px-2 py-2">Correlation mode: </a>
-                        <button className={mode == "time" ? activeButtonClass : inactiveButtonClass}
-                                onClick={() => setCorrelationMode("time")}>
-                            time
-                        </button>
-                        <button className={mode == "packets" ? activeButtonClass : inactiveButtonClass}
-                                onClick={() => setCorrelationMode("packets")}>
-                            packets
-                        </button>
-                    </div>
-                </div>
-            </div>
-            <div>
-            </div>
-            <div>
-                <ReactApexChart
-                    options={options}
-                    series={series}
-                    type="scatter"
-                />
-            </div>
-        </div>
-    );
+        <ReactApexChart
+            options={options}
+            series={series_out}
+            type="line"
+        />
+    )
 }
