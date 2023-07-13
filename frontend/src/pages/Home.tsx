47089c001282eded4ec7720a40c4adcf952c2d7b
Sijisu <mail@sijisu.eu>
2023-07-13 18:12:21 +0200

    Frontend: better shortcut reference

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,35 +1,49 @@
+const shortcutTableData = [
+  [
+    { key: 'j/k', action: 'Up/Down in FlowList' },
+    { key: 's', action: 'Focus search bar' },
+    { key: 'esc', action: 'Unfocus search bar' },
+    { key: 'i/o', action: 'Toggle flag in/out filters' },
+    { key: 'r', action: 'Refresh flows' },
+  ]
+];
+
+const generateShortcutTable = (data: { key: string; action: string; }[][]) => {
+  return (
+        <div className="flex flex-row gap-4">
+          {data.map((table, tableIndex) => (
+            <table key={tableIndex} className="border-collapse border border-slate-500 table-auto">
+              <thead>
+                <tr>
+                  <th className="border border-slate-600 px-4">Key</th>
+                  <th className="border border-slate-600 px-4">Action</th>
+                </tr>
+              </thead>
+              <tbody>
+              {table.map((row, rowIndex) => (
+                <tr key={rowIndex}>
+                  {Object.entries(row).map((cell, cellIndex) => (
+                    <td className="border border-slate-700 px-4" key={cellIndex}>
+                      {cell[1]}
+                    </td>
+                  ))}
+                </tr>
+              ))}
+              </tbody>
+            </table>
+          ))}
+        </div>
+  );
+};
+
+
 export function Home() {
   return (
     <div className="p-4 flex flex-col gap-4 justify-center items-center h-full opacity-40">
       <span className="text-9xl">🌷</span>
       <h1 className="text-5xl text-gray-600">Welcome to Tulip</h1>
       <h1 className="text-2xl text-gray-500">Shortcut reference:</h1>
-      <table className="border-collapse border border-slate-500 table-auto">
-        <thead>
-          <tr>
-            <th className="border border-slate-600 px-4">Key</th>
-            <th className="border border-slate-600 px-4">Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td className="border border-slate-700 px-4">j/k</td>
-            <td className="border border-slate-700 px-4">Up/Down in FlowList</td>
-          </tr>
-          <tr>
-            <td className="border border-slate-700 px-4">s</td>
-            <td className="border border-slate-700 px-4">Focus search bar</td>
-          </tr>
-          <tr>
-            <td className="border border-slate-700 px-4">esc</td>
-            <td className="border border-slate-700 px-4">Unfocus search bar</td>
-          </tr>
-          <tr>
-            <td className="border border-slate-700 px-4">i/o</td>
-            <td className="border border-slate-700 px-4">Toggle flag in/out filters</td>
-          </tr>
-        </tbody>
-      </table>
+      {generateShortcutTable(shortcutTableData)}
       {/* <h1 className="text-3xl font-bold pt-2 pb-4"></h1> */}
     </div>
   );
